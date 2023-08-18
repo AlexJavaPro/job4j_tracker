57f9954bd6946c09ebb04ea5e28f41ac9a7c1515
amajo <a.majoroff2016@gmail.com>
2023-08-18 08:39:45 +0300

    1.3. Вызов метода с аргументами. [#166086 #437472]

--- src/main/java/ru/job4j/oop/Jukebox.java
+++ src/main/java/ru/job4j/oop/Jukebox.java
@@ -2,26 +2,27 @@ package ru.job4j.oop;
 
 public class Jukebox {
 
-    private String songLetThemRunClumsily = "Пусть бегут неуклюже" + System.lineSeparator()
+    private final static String SONG_LET_THEM_RUN_CLUMSILY = "Пусть бегут неуклюже" + System.lineSeparator()
             + "Пешеходы по лужам," + System.lineSeparator()
             + "А вода — по асфальту рекой." + System.lineSeparator()
             + "И не ясно прохожим" + System.lineSeparator()
             + "В этот день непогожий," + System.lineSeparator()
             + "Почему я веселый такой.";
-    private String songGoodNight = "Спят усталые игрушки, книжки спят," + System.lineSeparator()
+    private final static String SONG_GOOD_NIGHT = "Спят усталые игрушки, книжки спят," + System.lineSeparator()
             + "Одеяла и подушки ждут ребят;" + System.lineSeparator()
             + "Даже сказка спать ложится," + System.lineSeparator()
             + "Чтобы ночью нам присниться." + System.lineSeparator()
             + "Ты ей пожелай - баю-бай.";
 
+    private final static String NOT_FOUND = "Песня не найдена";
+
     public void music(int position) {
-        if (position == 1) {
-            System.out.println(songLetThemRunClumsily);
-        } else if (position == 2) {
-            System.out.println(songGoodNight);
-        } else {
-            System.out.println("Песня не найдена");
-        }
+        String result = switch (position) {
+            case 1 -> SONG_LET_THEM_RUN_CLUMSILY;
+            case 2 -> SONG_GOOD_NIGHT;
+            default -> NOT_FOUND;
+        };
+        System.out.println(result);
     }
 
     public static void main(String[] args) {
