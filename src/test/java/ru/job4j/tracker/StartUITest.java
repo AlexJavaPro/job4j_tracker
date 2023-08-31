34a03239e4f77bfe78a3cbf539874c3a035dc90f
amajo <a.majoroff2016@gmail.com>
2023-08-31 21:39:55 +0300

    1.1. Тесты на StartUI. [#223313 #441359]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -173,11 +173,12 @@ public class StartUITest {
                         + "1. Exit program" + ln
         );
     }
+
     @Test
     public void whenInvalidExit() {
         Output out = new StubOutput();
         Input in = new StubInput(
-                new String[] {"1","0"}
+                new String[] {"1", "0"}
         );
         Tracker tracker = new Tracker();
         UserAction[] actions = new UserAction[]{
